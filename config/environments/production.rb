a73ad3716887b5a2890e56bca76f0cdabc72386a
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-01-20 17:38:39 -0600

    use right host for emails about passwords

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ MedicWatcher::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'watchman.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'watchman-cedar.herokuapp.com' }
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
